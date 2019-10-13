6b605d82c186dc5a33b20048389621ef96a0c973
manu <e.l.maurer@gmx.de>
2019-10-13 16:10:27 +0200

    nice

---
+++ app.py
@@ -0,0 +1,161 @@
+# -*- coding: utf-8 -*-
+
+import json
+import sys
+import os
+import stripe
+import aiohttp
+import asyncio
+import uvicorn
+import stripe
+from fastai import *
+from fastai.vision import *
+from io import BytesIO
+from starlette.applications import Starlette
+from starlette.middleware.cors import CORSMiddleware
+from starlette.responses import HTMLResponse, JSONResponse
+from starlette.staticfiles import StaticFiles
+from scripts import tabledef
+from scripts import forms
+from scripts import helpers
+from flask import Flask, redirect, url_for, render_template, request, session
+
+export_file_name = 'export.pkt'
+
+classes = ['fake', 'real']
+path = Path(__file__).parent
+
+async def setup_learner():
+    try:
+        learn = load_learner(path, export_file_name)
+        return learn
+    except RuntimeError as e:
+        if len(e.args) > 0 and 'CPU-only machine' in e.args[0]:
+            print(e)
+            message = "\n\nThis model was trained with an old version of fastai and will not work in a CPU environment.\n\nPlease update the fastai library in your training environment and export your model again.\n\nSee instructions for 'Returning to work' at https://course.fast.ai."
+            raise RuntimeError(message)
+        else:
+            raise
+
+app = Flask(__name__)
+app.secret_key = os.urandom(12)  # Generic key for dev purposes only
+
+
+stripe_keys = {
+  'secret_key': "sk_test_DNesneMw03u6msRRwDN66lb6001kRscmfg",  # Killian's keys, I should put this in an environment variable
+  'publishable_key': "pk_test_Eoego2U3SSlSNqCCMcKA7Sxx00urEsAkYE"
+}
+
+stripe.api_key = stripe_keys['secret_key']
+
+# Heroku
+#from flask_heroku import Heroku
+#heroku = Heroku(app)
+
+# ======== Routing =========================================================== #
+# -------- Login ------------------------------------------------------------- #
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if not session.get('logged_in'):
+        form = forms.LoginForm(request.form)
+        if request.method == 'POST':
+            username = request.form['username'].lower()
+            password = request.form['password']
+            if form.validate():
+                if helpers.credentials_valid(username, password):
+                    session['logged_in'] = True
+                    session['username'] = username
+                    return json.dumps({'status': 'Login successful'})
+                return json.dumps({'status': 'Invalid user/pass'})
+            return json.dumps({'status': 'Both fields required'})
+        return render_template('login.html', form=form)
+    user = helpers.get_user()
+    return render_template('home.html', user=user, key=stripe_keys['publishable_key'])
+
+
+@app.route("/logout")
+def logout():
+    session['logged_in'] = False
+    return redirect(url_for('login'))
+
+
+
+@app.route('/analyze', methods=['POST'])
+async def analyze(request):
+    img_data = await request.form()
+    img_bytes = await (img_data['file'].read())
+    img = open_image(BytesIO(img_bytes))
+    prediction = learn.predict(img)[0]
+
+    return render_template('image_upload.html', predictions=prediction)
+
+    #return JSONResponse({'result': str(prediction)})
+
+
+@app.route('/image_upload')
+def image_upload():
+	return render_template('image_upload.html', predictions=[])
+
+
+# -------- Signup ---------------------------------------------------------- #
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if not session.get('logged_in'):
+        form = forms.LoginForm(request.form)
+        if request.method == 'POST':
+            username = request.form['username'].lower()
+            password = helpers.hash_password(request.form['password'])
+            email = request.form['email']
+            if form.validate():
+                if not helpers.username_taken(username):
+                    helpers.add_user(username, password, email)
+                    session['logged_in'] = True
+                    session['username'] = username
+                    return json.dumps({'status': 'Signup successful'})
+                return json.dumps({'status': 'Username taken'})
+            return json.dumps({'status': 'User/Pass required'})
+        return render_template('login.html', form=form)
+    return redirect(url_for('login'))
+
+
+# -------- Settings ---------------------------------------------------------- #
+@app.route('/settings', methods=['GET', 'POST'])
+def settings():
+    if session.get('logged_in'):
+        if request.method == 'POST':
+            password = request.form['password']
+            if password != "":
+                password = helpers.hash_password(password)
+            email = request.form['email']
+            helpers.change_user(password=password, email=email)
+            return json.dumps({'status': 'Saved'})
+        user = helpers.get_user()
+        return render_template('settings.html', user=user)
+    return redirect(url_for('login'))
+
+#============== Stripe ========================================================#
+
+@app.route('/charge', methods=['POST'])
+def charge():
+
+    # amount in cents
+    amount = 500  # I don't know how much the pricing should be ...
+
+    customer = stripe.Customer.create(
+        email=request.form['stripeEmail'],
+        source=request.form['stripeToken']
+    )
+
+    stripe.Charge.create(
+        customer=customer.id,
+        amount=amount,
+        currency='usd',
+        description='Flask Charge'
+    )
+
+    return redirect(url_for('image_upload'))
+
+
+# ======== Main ============================================================== #
+if __name__ == "__main__":
+    app.run(debug=True, use_reloader=True)
